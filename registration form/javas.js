2563799573bcfd7ba4b6e992e1d6ea6764117f9d
Sakshi Jogdand <sakshijog7774@gmail.com>
2024-12-11 12:02:47 +0530

    change

---
+++ registration form/javas.js
@@ -0,0 +1,8 @@
+document.getElementById('otp-button').addEventListener('click', function() {
+    alert('OTP sent to your phone number!');
+});
+
+document.getElementById('registration-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+});
